d78cb2038b4aeef815ad32b35a68d7230a7479aa
Michael Richards <m.richards@utexas.edu>
2023-03-04 00:24:45 -0600

    added keyword generation

--- htc_bill_backend/script.py
+++ htc_bill_backend/script.py
@@ -17,6 +17,7 @@ def main():
     text = soup.get_text(strip=True)
     summary_str = "Please summarize the following piece of legislation in at most a paragraph and pretend you are presenting it to a group of middle school students. The legislation is: {}".format(text)
     title_str = "Please create a title for the following piece of legislation. Make the title engaging and no longer than 10 words. The legislation is: {}".format(text)
+    keyword_str = "Please produce 5 single word keywords in csv format for the following piece of legislation: {}".format(text)
     
     #get responses from gpt3 based on the prompts
     response_summary = openai.ChatCompletion.create(
@@ -27,10 +28,15 @@ def main():
     model="gpt-3.5-turbo", 
     messages=[{"role": "user", "content": title_str }],
     )
+    response_keyword = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo", 
+    messages=[{"role": "user", "content": keyword_str }],
+    )
     bill = dict()
     #populate the dictionary for the bill object
     bill["title"] = response_title.get("choices")[0].get("message").get("content")
     bill["description"] = response_summary.get("choices")[0].get("message").get("content")
+    bill["keywords"] = [x.strip() for x in response_keyword.get("choices")[0].get("message").get("content").split(',')]
 
     mongo_str = "mongodb+srv://ynchen2829:"+mongo_clientkey+"@htc-data-cluster0.tmj9bij.mongodb.net/?retryWrites=true&w=majority"
     print(mongo_str)
